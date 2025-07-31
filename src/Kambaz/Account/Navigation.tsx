7bdcf6533a360fc50d111e6c69b31823ca176024
Anirudh938 <revallianirudh@gmail.com>
2025-07-31 15:03:49 -0400

    assignment 5

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Link } from "react-router-dom";;
+import { Link } from "react-router-dom";
 
 export default function AccountNavigation() {
     return (
